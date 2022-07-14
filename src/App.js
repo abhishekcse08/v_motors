2b49377669c7670064173f894a8fb22edcd19ff7
abhishekcse08 <abhishekcse08@gmail.com>
2022-07-15 00:12:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,56 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 //import './components/ExpenseItem'
 //import ExpenseItem from './components/ExpenseItem';
-import {
-  MDBNavbar,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBNavbarToggler,
-  MDBContainer,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+
 function App() {
   return (
-    <header>
-      <MDBNavbar expand='lg' light bgColor='white'>
-        <MDBContainer fluid>
-          <MDBNavbarToggler
-            aria-controls='navbarExample01'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
-          >
-            <MDBIcon fas icon='bars' />
-          </MDBNavbarToggler>
-          <div className='collapse navbar-collapse' id='navbarExample01'>
-            <MDBNavbarNav right className='mb-2 mb-lg-0'>
-              <MDBNavbarItem active>
-                <MDBNavbarLink aria-current='page' href='#'>
-                  Home
-                </MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Dealers</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Segments</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>About</MDBNavbarLink>
-              </MDBNavbarItem>
-            </MDBNavbarNav>
-          </div>
-        </MDBContainer>
-      </MDBNavbar>
-
-      <div className='p-5 text-center bg-light'>
-        <h1 className='mb-3'>Heading</h1>
-        <h4 className='mb-3'>Subheading</h4>
-        <a className='btn btn-primary' href='' role='button'>
-          Call to action
-        </a>
-      </div>
-    </header>
+    <div className="App">
+     <p>Vasundhara Motors Sultanpur</p>
+     
+    </div>
   );
 }
 
