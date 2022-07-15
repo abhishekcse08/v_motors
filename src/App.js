6127c2a298e2d199c793006977f83a0dbbdb1b5a
abhishekcse08 <abhishekcse08@gmail.com>
2022-07-15 16:25:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+
 //import './components/ExpenseItem'
 //import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
-     <p>Vasundhara Motors Sultanpur</p>
      
+     <Home />
     </div>
+    
   );
 }
 
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,35 @@
-
-function HomePage(){
+import '../styles/Home.css';
+function Home(){
 
 return(
-       <div></div>
+       <div>
+
+<header>
+  <h2>Vasundhara Motors Sultanpur</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">Sultanpur</a></li>
+      <li><a href="#">Amethi</a></li>
+      <li><a href="#">Mirzapur</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>Site is under maintinance</h1>
+    <p>Please wait</p>
+    <p>We are working on it</p>
+  </article>
+</section>
+
+<footer>
+  <p>Please contact Abhishek</p>
+</footer>
+
+       </div>
 );
 }
 
-export default HomePage;
+export default Home;
